6632c8679ccd128a1260b56135a04f417e7552e3
Dima <74459076+diamam90@users.noreply.github.com>
2021-03-04 00:01:13 +0300

    111

--- src/calculatorModel/Calculator.java
+++ src/calculatorModel/Calculator.java
@@ -4,16 +4,22 @@ public class Calculator<T extends Number> {
 	private double result;
 
 	public double addition(int a, int b) {
-		return result = a + b;
+		result = a + b;
+		System.out.println("Результат сложения чисел "+a+" и "+b+ " : "+result);
+		return result;
 	}
 
 	public double subtraction(int a, int b) {
-		return result = a - b;
+		result = a - b;
+		System.out.println("Результат вычитания чисел "+a+" и "+b+ " : "+result);
+		return result;
 	}
 
 	public double division(int a, int b) {
 		if (b != 0) {
-			return result = a / b;
+			result = a / b;
+			System.out.println("Результат деления чисел "+a+" и "+b+ " : "+result);
+			return result;
 		} else {
 			System.out.println("На ноль делить нельзя!");
 			return -1;
@@ -21,7 +27,11 @@ public class Calculator<T extends Number> {
 	}
 
 	public double multiplication(int a, int b) {
-		return result = a * b;
+		result = a * b;
+		System.out.println("Результат умножения чисел "+a+" и "+b+ " : "+result);
+		return result;
 	}
-
+	public double getResult() {
+		return result;
+	}
 }
--- src/calculatorModel/InteractRunner.java
+++ src/calculatorModel/InteractRunner.java
@@ -1,18 +1,71 @@
 package calculatorModel;
-import java.io.*;
+import java.util.Scanner;
 public class InteractRunner {
-	int a,b;
+	private int a,b;
+	private int start, end;
 	public static void main(String[] args) {
 		
-		System.out.println("Выберите операцию калькулятора");
-		System.out.println("1 - Сложение \n2 - Вычитание \n3 - Умножение \n4 - Деление \n");
+		
 		InteractRunner ir = new InteractRunner();
-	
+		Scanner scan = new Scanner(System.in);
+		int operation = ir.inputOperation(scan);
 		
 		Calculator calc1 = new Calculator();
 		
-		}
+		int number1 = ir.inputNumber(scan);
+		int number2 = ir.inputNumber(scan);
+		ir.calcMethod(operation, calc1, number1,number2);
+	}
+
+	
+	
+	public void calcMethod(int operationId,Calculator calc,int number1,int number2) {
 		
+		switch (operationId) {
+		case 1: 
+			calc.addition(number1, number2);
+			break;
+		case 2: 
+			calc.subtraction(number1, number2);
+			break;
+		case 3: 
+			calc.multiplication(number1, number2);
+			break;
+		case 4: 
+			calc.division(number1, number2);
+			break;}
+	}
+	
+	public int inputOperation(Scanner scan) {
+		System.out.println("Выберите операцию калькулятора");
+		System.out.println("1 - Сложение \n2 - Вычитание \n3 - Умножение \n4 - Деление \n5 - Произвести операцию с полученным результатом \n6 - Очистить \n7 - Выйти из калькулятора");
+		int choice=0;
+		while (scan.hasNext()) {
+			if (scan.hasNextInt()) {
+				choice=scan.nextInt();
+				if (choice>=1 && choice<=7) {
+					break;
+				}
+			}
+		scan.nextLine();	
+		}
+		System.out.println("Вы выбрали: " +choice);
+		return choice;
+	}
+		
+	public int inputNumber(Scanner scan) {
+		System.out.println("Введите целое число");
+		int number=0;
+		while (scan.hasNext()) {
+			if (scan.hasNextInt()) {
+				number=scan.nextInt();
+				break;
+			}
+		scan.nextLine();
+		}
+		System.out.println("Введенное число: " + number);
+		return number;
+	}
 	
 
 	}
