2475ccb0ff3bc29fe38d201f584a47e61d67901f
Dima <74459076+diamam90@users.noreply.github.com>
2021-03-08 22:20:28 +0300

    worked!

--- src/calculatorModel/InteractRunner.java
+++ src/calculatorModel/InteractRunner.java
@@ -4,8 +4,10 @@ public class InteractRunner {
 	private int number1,number2;
 	private Calculator calc;
 	private boolean start=true;
-	private boolean resultIsEmpty=true;
-	private boolean hasPreviousOperation=false;
+	private boolean hasResult =false;
+	private boolean chosenPrevOp =false;
+	private UI ui;
+
 	InteractRunner(Calculator calc){this.calc=calc;}
 	public static void main(String[] args) {
 				
@@ -18,38 +20,45 @@ public class InteractRunner {
 		Scanner scan = new Scanner(System.in);
 		while (isStart()) {
 			int operation = inputOperation(scan);
-			if (operation==5) {
-				if (hasPreviousOperation) {
-					hasPreviousOperation=false;
+			if (operation==5 && hasResult) {
+				if (!chosenPrevOp) {
+					chosenPrevOp = true;
 					continue;
 				}
-				else {System.out.println("Данная операция недоступна \nВыберите другую операцию");
-						operation = inputOperation(scan);					
-						continue;
+				else {
+					System.out.println("Данная операция недоступна.");
+					continue;
 				}
-			} 
+			}
+			if (operation ==5 && !hasResult){
+				System.out.println("Данная операция недоступна.");
+						//operation = inputOperation(scan);
+						continue;
+			}
+
 			if (operation==6) {
 				calc.clearResult();
-				resultIsEmpty=false;
+				hasResult =false;
 				continue;
-				}
-			if (operation>=1 && operation <=4 && !hasPreviousOperation) {
+			}
+			if ((operation>=1 && operation <=4) && (!chosenPrevOp | !hasResult) ){
 				System.out.println("Введите первое число:");
 				number1=inputNumber(scan);
 				System.out.println("Введите второе число:");
 				number2=inputNumber(scan);
 				calcMethod(operation);
-				resultIsEmpty=false;
+				hasResult =true;
 				continue;
 				
 			}
-			if (operation>=1 && operation <=4 && hasPreviousOperation){
+			if (operation>=1 && operation <=4 && chosenPrevOp){
 				System.out.println("первое число:" +calc.getResult());
 				number1=(int)calc.getResult();
 				System.out.println("Введите второе число:");
 				number2=inputNumber(scan);
 				calcMethod(operation);
-				resultIsEmpty=true;
+				hasResult =true;
+				chosenPrevOp=false;
 				continue;
 			}
 			
@@ -71,20 +80,16 @@ public class InteractRunner {
 	public void calcMethod(int operationId) {
 		
 		switch (operationId) {
-		case 1: 
-			System.out.println("Операция сложение");
+		case 1:
 			calc.addition(number1, number2);
 			break;
-		case 2: 
-			System.out.println("Операция вычитание");
+		case 2:
 			calc.subtraction(number1, number2);
 			break;
-		case 3: 
-			System.out.println("Операция умножение");
+		case 3:
 			calc.multiplication(number1, number2);
 			break;
-		case 4: 
-			System.out.println("Операция деление");
+		case 4:
 			calc.division(number1, number2);
 			break;}
 	}
@@ -94,12 +99,29 @@ public class InteractRunner {
 		System.out.println("Выберите операцию");
 		System.out.println("-----------------");
 		int choice=0;
-		if (hasPreviousOperation) {System.out.println("1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \n5.Операция с предыдущим результатом \n6.Обнулить \n7.Выход из калькулятора");}
-		if (!hasPreviousOperation) {System.out.println("1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \n6.Обнулить \n7.Выход из калькулятора");}
+		if (hasResult && !chosenPrevOp) {System.out.println("1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \n5.Операция с предыдущим результатом \n6.Обнулить \n7.Выход из калькулятора");}
+		if (!hasResult | chosenPrevOp) {System.out.println("1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \n6.Обнулить \n7.Выход из калькулятора");}
 			while (scan.hasNext()) {
 				if (scan.hasNextInt()) {
 					choice=scan.nextInt();
 					if (choice>=1 && choice<=7) {
+						switch (choice){
+							case 1: System.out.println("Вы выбрали операцию сложение");
+							break;
+							case 2: System.out.println("Вы выбрали операцию вычитание");
+								break;
+							case 3: System.out.println("Вы выбрали операцию умножение");
+								break;
+							case 4: System.out.println("Вы выбрали операцию деление");
+								break;
+							case 5: System.out.println("Вы выбрали операцию с предыдущим результатом");
+								break;
+							case 6: System.out.println("Вы выбрали операцию обнуление калькулятора");
+								break;
+							case 7: System.out.println("Вы выбрали выход из калькулятора");
+								break;
+
+						}
 						break;
 					}
 					else System.out.println("Выберите одну из операций");
